ac0ce2226b7f36fd96e9cc7f58a76f8512a08acd
Ankit Ranjan <62400447+i-ankit-25@users.noreply.github.com>
2026-05-19 15:24:51 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int valueToSearch = target - nums[i];
+            Integer previousIndex = numMap.get(valueToSearch);
+            if(previousIndex != null){//We check before inserting to avoid using the same element twice.
+                return new int[]{previousIndex, i};
+            }
+            numMap.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No Solution Found");
+    }
+}
